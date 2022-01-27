f2082e6aa4ad411208c2f1eea220dff5f8bffc13
gby <nameGBY@163.com>
2022-01-27 23:42:05 +0800

    init():初始化客户端

---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--主页面布局-->
+  <!-- 后续加 -->
+  <!-- <header></header> -->
+  <main></main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import main from "@/views/main/main.vue";
+export default defineComponent({
+  name: "",
+  components: {
+    main,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ client/src/views/main/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
